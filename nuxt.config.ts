84ed2925b203c4c1a185a14d33556b1e98867c8d
MAW PAING THU - COMSOL <mawpaingthu@comradesol.com>
2024-07-12 15:05:16 +0900

    Favicon にbaseUrl設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const BASE_URL = "/ga/"
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   app: {
-    baseURL: '/ga/',
+    baseURL: BASE_URL,
     head:{
       link:[
-        { rel: 'icon', type:'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type:'image/x-icon', href: `${BASE_URL}favicon.ico` }
       ]
     }
   }
